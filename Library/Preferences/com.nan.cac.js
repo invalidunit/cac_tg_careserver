e272518653909aecc82216ba064c33d4e16ab902
zhaonan <35675770+invalidunit@users.noreply.github.com>
2025-02-26 11:08:45 +0800

    Create com.nan.cac.js

---
+++ Library/Preferences/com.nan.cac.js
@@ -0,0 +1,15 @@
+{
+  "app": [
+    {
+      "enable": 1,
+      "name": "CareServer",
+      "url": "https://apps.apple.com/cn/app/id6474921250"
+    }
+  ],
+  "enable": 1,
+  "message": {
+    "cache": "CareServer\\n当前最新版本：\\napp_version\\n版本 id 为：\\napp_version_id\\n已缓存到本地等待下一次对比",
+    "del": "CareServer\\n可能已下架，已连续 10 次获取信息失败，最后出现版本为：\\napp_version",
+    "up": "CareServer\\n发现版本变更：\\napp_version\\n点击前往 AppStore 更新：\\nhttps://apps.apple.com/app/id6474921250"
+  }
+}
